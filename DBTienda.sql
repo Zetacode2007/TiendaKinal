74f34fbc1b7d0a71e1ace5fc6cac6be8fb68ae7d
Zetacode2007 <fernandozeta057@gmail.com>
2025-07-14 21:18:57 -0600

    Primer commit

---
+++ DBTienda.sql
@@ -0,0 +1,2 @@
+create database DBTiendaKinal;
+use DBTiendaKinal;
---
+++ TiendaKinal_T/src/main/java/TiendaKinal_T.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Familia Zeta
+ */
+public class TiendaKinal_T {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
