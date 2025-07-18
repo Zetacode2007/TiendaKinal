d56d40470480436cae02a421e033415cbc71843d
Zetacode2007 <fernandozeta057@gmail.com>
2025-07-17 23:09:08 -0600

    Vista de empleados terminada

--- TiendaKinal_T/src/main/java/org/josezeta/system/Main.java
+++ TiendaKinal_T/src/main/java/org/josezeta/system/Main.java
@@ -8,65 +8,66 @@ import javafx.fxml.Initializable;
 import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.josezeta.controller.ClienteController;
 import org.josezeta.controller.InicioController;
 import org.josezeta.controller.MenuPrincipalController;
 
+public class Main extends Application {
 
-public class Main extends Application{
     private static String URL = "/org/josezeta/view/";
     private Stage escenarioPrincipal;
     private Scene siguienteEscena;
- 
+
     public static void main(String[] args) {
         launch(args);
     }
- 
+
     @Override
     public void start(Stage stage) throws Exception {
         this.escenarioPrincipal = stage;
         getInicioView();
         stage.show();
     }
- 
+
     public Initializable cambiarEscena(String fxml, double ancho, double alto) throws Exception {
         Initializable interfazDeCambio = null;
         FXMLLoader cargadorFXML = new FXMLLoader();
         InputStream archivoFXML = Main.class.getResourceAsStream(URL + fxml);
-            cargadorFXML.setBuilderFactory(new JavaFXBuilderFactory());
+        cargadorFXML.setBuilderFactory(new JavaFXBuilderFactory());
         cargadorFXML.setLocation(Main.class.getResource(URL + fxml));
-        
-        siguienteEscena = new Scene(cargadorFXML.load(archivoFXML),ancho,alto);
+
+        siguienteEscena = new Scene(cargadorFXML.load(archivoFXML), ancho, alto);
         escenarioPrincipal.setScene(siguienteEscena);
         escenarioPrincipal.sizeToScene();//se acopla a las dimensiones de la escena
-        
+
         //Configurar interfaz Initializable
         interfazDeCambio = cargadorFXML.getController();
         return interfazDeCambio;
     }
-    
+
     public void getInicioView() {
         try {
-            InicioController control = (InicioController) cambiarEscena("InicioView.fxml",600,400);
+            InicioController control = (InicioController) cambiarEscena("InicioView.fxml", 600, 400);
             control.setPrincipal(this);
         } catch (Exception ex) {
-            System.out.println("Error al ir al inicio: "+ ex);
+            System.out.println("Error al ir al inicio: " + ex);
             ex.printStackTrace();
         }
     }
-    
+
     public void getMenuPrincipalView() {
         try {
-            MenuPrincipalController control = (MenuPrincipalController) cambiarEscena("MenuPrincipalView.fxml",631.5,388);
+            MenuPrincipalController control = (MenuPrincipalController) cambiarEscena("MenuPrincipalView.fxml", 631.5, 388);
             control.setPrincipal(this);
         } catch (Exception ex) {
-            System.out.println("Error al ir al Menu Principal: "+ex);
+            System.out.println("Error al ir al Menu Principal: " + ex);
             ex.printStackTrace();
         }
     }
-    
-    public void getClientesView(){
+
+    public void getClientesView() {
         try {
-            MenuPrincipalController control = (MenuPrincipalController) cambiarEscena("ClientesView.fxml",400,350);
+            ClienteController control = (ClienteController) cambiarEscena("ClientesView.fxml", 1000, 705);
             control.setPrincipal(this);
         } catch (Exception e) {
             System.out.println("Error al ir a Clientes" + e);
